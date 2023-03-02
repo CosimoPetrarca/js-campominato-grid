7222612f984a59ecf594bb405ebf97217e3755b0
P4anix <pancok_1@yahoo.it>
2023-03-02 18:00:07 +0100

    main

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 'Use strict'
+
+// Seleziono l'elemento HTML con ID 
+const gameGrid = document.getElementById('griglia');
+
+// Definisco una funzione per creare la griglia di gioco
+function createGameGrid() {
+  // Utilizzo un ciclo for per creare 100 celle di gioco
+  for (let i = 1; i <= 100; i++) {
+    // Creo un nuovo elemento div per ogni cella di gioco
+    const cell = document.createElement('div');
+    // Aggiungo il numero della cella come testo all'interno della cella stessa
+    cell.textContent = i;
+    // Aggiungo la cella alla griglia di gioco utilizzando il metodo append di JavaScript
+    gameGrid.append(cell);
+
+    // Aggiungo un evento click a ogni cella di gioco
+    cell.addEventListener('click', function() {
+        const element = document.querySelector('.blue');
+        cell.classList.toggle('blue');
+    });
+  }
+}
+
+// Chiamiamo la funzione createGameGrid per creare la griglia di gioco
+createGameGrid();
