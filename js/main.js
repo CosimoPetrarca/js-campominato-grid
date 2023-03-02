a54e886cc56bc6289f9004f2bb1d1ef1b91c8759
P4anix <pancok_1@yahoo.it>
2023-03-02 20:56:21 +0100

    button appair grid

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
 'Use strict'
 
 // Seleziono l'elemento HTML con ID 
-const gameGrid = document.getElementById('griglia');
+const gameGrid = document.querySelector('.griglia');
+
+// Selezioniamo il bottone e la griglia
+let bottone = document.querySelector("button");
+
+// Quando il bottone viene cliccato, mostriamo la griglia
+bottone.addEventListener("click", function() {
+    let appairGriglia = document.querySelector('.griglia');
+    appairGriglia.classList.toggle('show');
+});
 
 // Definisco una funzione per creare la griglia di gioco
 function createGameGrid() {
-  // Utilizzo un ciclo for per creare 100 celle di gioco
-  for (let i = 1; i <= 100; i++) {
-    // Creo un nuovo elemento div per ogni cella di gioco
-    const cell = document.createElement('div');
-    // Aggiungo il numero della cella come testo all'interno della cella stessa
-    cell.textContent = i;
-    // Aggiungo la cella alla griglia di gioco 
-    gameGrid.append(cell);
+    // Utilizzo un ciclo for per creare 100 celle di gioco
+    for (let i = 1; i <= 100; i++) {
+        // Creo un nuovo elemento div per ogni cella di gioco
+        const cell = document.createElement('div');
+        // Aggiungo il numero della cella come testo all'interno della cella stessa
+        cell.textContent = i;
+        // Aggiungo la cella alla griglia di gioco 
+        gameGrid.append(cell);
 
-    // Aggiungo un evento click a ogni cella di gioco
-    cell.addEventListener('click', function() {
-        const element = document.querySelector('.blue');
-        cell.classList.toggle('blue');
-    });
-  }
+        // Aggiungo un evento click a ogni cella di gioco
+        cell.addEventListener('click', function () {
+            const element = document.querySelector('.blue');
+            cell.classList.toggle('blue');
+            alert(i);
+        });
+    }
 }
 
 // Chiamiamo la funzione createGameGrid per creare la griglia di gioco
